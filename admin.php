b021cc7e3e8b95ebf93bf471a50c208affbe635b
eccenux <eccenux+git@gmail.com>
2025-03-19 05:22:00 +0100

    Update host naming conventions

--- admin.php
+++ admin.php
@@ -1,12 +1,6 @@
 <?php
 /**
 	Aktywność adminów na plwiki.
-
-	Kiedy dany admin/user:
-	- wykonał ostatnią edycję w przestrzeni głównej (ns:0).
-	- wykonał ostatnią edycję w przestrzeni MediaWiki: (ns:8).
-	- wykonał ostatnią logowaną akcję administracyjną (last log action).
-	- wykonał ostatnią edycję zablokowanej strony (o ile da się to sprawdził).
 */
 /**/
 $arrMyCnf = parse_ini_file("../../.my.script.cnf", true);
@@ -14,8 +8,9 @@ $arrSrcDb = $arrMyCnf['plwikidb'];
 
 $dbConfig = [];
 // $arrSrcDb['host'], $arrSrcDb['dbname']
-$dbConfig['host'] = "plwiki.labsdb";
-$dbConfig['database'] = "plwiki_p";
+$project = "plwiki";
+$dbConfig['host'] = $project.".web.db.svc.wikimedia.cloud";
+$dbConfig['database'] = $project."_p";
 $dbConfig['user'] = $arrSrcDb['user'];
 $dbConfig['password'] = $arrSrcDb['password'];
 /**/
